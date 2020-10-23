949151b26e913342599250d829429204963b44f0
yangja1 <61275050+yangja1@users.noreply.github.com>
2020-10-23 09:16:35 +0100

    Update LCATest.java
    Improvements in unit tests. Still some errors remaining unsolved. e.g. The method assertTrue(boolean) is ambiguous for the type LCATest

--- LCA/src/LCATest.java
+++ LCA/src/LCATest.java
@@ -1,13 +1,77 @@
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.*;
 import org.junit.jupiter.api.Test;
 
-class LCATest {
+public class LCATest {
 
 	@Test
-	void test() {
-		LCA test = new LCA();
-		int output = test.findLCA(2,3);
-		assertEquals(2, 3);
+	public void testFindLCA() {
+		LCA tree = new LCA();
+		tree.root = new Node(9);
+		tree.root.left = new Node(6);
+		tree.root.right = new Node(2);
+		tree.root.left.left = new Node(5);
+		tree.root.left.right = new Node(3);
+		tree.root.right.left = new Node(1);
+		tree.root.right.right = new Node(8);
+		/*
+		 * 9 / \ 6 2 / \ / \ 5 3 1 8
+		 */
+		assertEquals(6, tree.findLCA(5, 3));
+		assertEquals(6, tree.findLCA(6, 3));
+		assertEquals(9, tree.findLCA(5, 8));
+		assertNotEquals(2, tree.findLCA(9, 8));
+		assertNotEquals(6, tree.findLCA(1, 3));
+		assertNotEquals(9, tree.findLCA(1, 8));
 	}
+
+	@Test // as the findLCA function already runs the findLCAInternal function,
+	// there should be no errors in the findLCAInternal function.
+	// Regardless, here is some unit testing code.
+	public void testFindLCAInternal() {
+		LCA tree = new LCA();
+		tree.root = new Node(53);
+		tree.root.left = new Node(61);
+		tree.root.right = new Node(29);
+		tree.root.left.left = new Node(45);
+		tree.root.left.right = new Node(73);
+		tree.root.right.left = new Node(31);
+		tree.root.right.right = new Node(80);
+		/*
+		 * 53 / \ 61 29 / \ / \ 45 73 31 80
+		 */
+		assertEquals(53, tree.findLCAInternal(tree.root, 53, 31));
+		assertEquals(53, tree.findLCAInternal(tree.root, 29, 73));
+		// assertEquals(61,tree.findLCAInternal(tree.root, 45, 73));
+		// assertNotEquals(53,tree.findLCAInternal(tree.root,80,31));
+		// assertNotEquals(53,tree.findLCAInternal(tree.root, 29, 80));
+		// the above 3 statements don't work. There is an error insisting
+		// that 53 is the LCA. It should be the tree.root part causing the error.
+		assertNotEquals(61, tree.findLCAInternal(tree.root, 31, 73));
+	}
+
+	@Test
+	public void testFindPath() {
+		LCA tree = new LCA();
+		tree.root = new Node(18);
+		tree.root.left = new Node(35);
+		tree.root.right = new Node(86);
+		tree.root.left.left = new Node(38);
+		tree.root.left.right = new Node(94);
+		tree.root.right.left = new Node(93);
+		tree.root.right.right = new Node(36);
+		List<Integer> path = new ArrayList<>();
+		path.add(tree.root.data);
+		/*
+		 * 18 / \ 35 86 / \ / \ 38 94 93 36
+		 */
+		assertTrue(1 == 1); // java.lang.Error: Unresolved compilation problems:
+		// The method assertTrue(boolean) is ambiguous for the type LCATest
+		assertTrue(tree.findPath(tree.root, 35, path));
+	}
+
 }
